e302db4be6e4329c0444f0b5498ffc6eed51e2e3
Naoto Kato <my.dog.walk.on.air@gmail.com>
2025-03-01 23:02:48 +0900

    go

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from openai import OpenAI
 import openai
 api_key = os.getenv('OPENAI_API_KEY')
-client = OpenAI(api_key=api_key)
+#client = OpenAI(api_key=api_key)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
