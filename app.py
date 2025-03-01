fa32685f69925ff101fb37e8bb645e2ea480b3e6
Naoto Kato <my.dog.walk.on.air@gmail.com>
2025-03-01 22:42:14 +0900

    first commit.

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 
 from dotenv import load_dotenv
 load_dotenv()
-import streamlit as st
 
-st.title("サンプルアプリ①: 簡単なWebアプリ")
-
-input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
-
-text_count = len(input_message)
-
-if st.button("実行"):
-
-    st.write(f"文字数: **{text_count}**")
-    print("a")
 
 from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
